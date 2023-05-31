ed6c919910add779221b3c2a784885436d874084
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2023-06-01 01:56:54 +0700

    feat: add material ui to the application, removed unused css and api files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,31 @@
-import '@/styles/globals.css';
+import { CacheProvider, EmotionCache, ThemeProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
+import createEmotionCache from '../config/createEmotionCache';
+import theme from '../config/theme';
+
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
     <>
-      <Component {...pageProps} />;
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />;
+        </ThemeProvider>
+      </CacheProvider>
     </>
   );
 }
